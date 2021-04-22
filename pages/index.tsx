1edaac2937af6be5f0fa63f408382c75a276abf6
aprilguy <developer.jeong@outlook.com>
2021-04-22 21:53:03 +0900

    apply tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,18 @@
-import { GetServerSideProps, NextPage } from 'next'
+import Head from 'next/head'
 
-interface Props {
-  launch: {
-    mission: string
-    site: string
-    timestamp: number
-    rocket: string
-  }
-}
-const IndexPage: NextPage<Props> = ({ launch }) => {
-  const date = new Date(launch.timestamp)
+export default function Home() {
   return (
-    <main>
-      <h1>Next SpaceX Launch: {launch.mission}</h1>
-      <p>
-        {launch.rocket} will take off from {launch.site} on {date.toDateString()}
-      </p>
-    </main>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>NextJs Tutorial</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="text-6xl font-bold">
+        Welcome to{' '}
+        <a className="text-blue-600" href="https://nextjs.org">
+          Next.js!
+        </a>
+      </h1>
+    </div>
   )
-}
-export default IndexPage
-
-/*
- * More information about getServerSideProps:
- * https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
- */
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const response = await fetch('https://api.spacexdata.com/v3/launches/next')
-  const nextLaunch = await response.json()
-  return {
-    props: {
-      launch: {
-        mission: nextLaunch.mission_name,
-        site: nextLaunch.launch_site.site_name_long,
-        timestamp: nextLaunch.launch_date_unix * 1000,
-        rocket: nextLaunch.rocket.rocket_name,
-      },
-    },
-  }
 }
